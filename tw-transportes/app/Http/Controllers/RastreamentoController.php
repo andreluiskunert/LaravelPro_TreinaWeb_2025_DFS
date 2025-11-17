7750bc10cfe949ba308c10ed2b6892b1c0211577
Andre Luis Kunert <seuemail@example.com>
2025-11-16 21:46:05 -0300

    Bora Codar Com LaravelPro_2025

--- tw-transportes/app/Http/Controllers/RastreamentoController.php
+++ tw-transportes/app/Http/Controllers/RastreamentoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Frete;
 use Illuminate\Http\Request;
 
 class RastreamentoController extends Controller
@@ -11,7 +12,12 @@ class RastreamentoController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $frete = Frete::where('codigo_rastreio', 'TESTE123')->first();
         // 
-        return view('frete.rastreamento');
+        dd($frete);
+        return view('frete.rastreamento', [
+            'frete' => $frete
+        ]
+    );
     }
 }
