04fb6d378cad395f5a6682b3c0235c900b187884
Andre Luis Kunert <seuemail@example.com>
2025-11-21 08:14:56 -0300

    Bora Lá Codar com LaravelPro_2025

--- tw-transportes/app/Models/Cliente.php
+++ tw-transportes/app/Models/Cliente.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cliente extends Model
 {
-    //
+    //Sexta-feira, 21/11/2025 de manhã;
+    public function enviados():HasMany{
+        return $this->hasMany(Frete::class, 'remetente_id');
+    }
+    public function recebidos():HasMany{
+        return $this->hasMany(Frete::class, 'destinario_id');
+    }
 }
--- tw-transportes/routes/web.php
+++ tw-transportes/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\HistoricoController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RastreamentoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 Route::get('/rastreamento', RastreamentoController::class)->name('frete.rastreamento');
+//  Sexta-feira,21/11/2025 de Manhã
+Route::get('/historico', HistoricoController::class)->name('frete.historico');
 
