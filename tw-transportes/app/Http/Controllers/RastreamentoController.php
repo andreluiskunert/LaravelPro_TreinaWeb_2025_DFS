050c772be987e602a18b070319d97e04cbd6197a
Andre Luis Kunert <seuemail@example.com>
2025-11-20 21:49:27 -0300

    Bora Codar com LaravelPro_2025

--- tw-transportes/app/Http/Controllers/RastreamentoController.php
+++ tw-transportes/app/Http/Controllers/RastreamentoController.php
@@ -13,11 +13,14 @@ class RastreamentoController extends Controller
     public function __invoke(Request $request)
     {   
         // Quarta-feira, 19/11/2025 de Noite
-        $codigoRastreio = $request->input('codigo_rastreio');
+        $codigoRastreio = $request->input('codigo_rastreio', '');
         $frete = Frete::where('codigo_rastreio', $codigoRastreio)
         ->with('etapas')
         ->first();
         // 
+        if(! $frete) {
+            return redirect()->back()->with('error', 'frete não encontrado....');
+        }
     
         return view('frete.rastreamento', [
             'frete' => $frete
--- tw-transportes/resources/views/components/layout.blade.php
+++ tw-transportes/resources/views/components/layout.blade.php
@@ -20,6 +20,14 @@
                 </a>
             </div>
         </nav>
+            @if (session('error'))
+            <div class="container mx-auto px-4 pt-20"> 
+                <div class="bg-red-100 border border-r-red-400 text-red-700 px-4 py-3 rounded-relaite" role="alert"></div>
+              <strong class="font-bold">Erro!</strong>
+              <span class="block sm:inline"> {{ session('error')}} </span>
+
+            </div>
+            @endif
 
         <div class="container mx-auto px-4 pt-16">
           {{ $slot }}
